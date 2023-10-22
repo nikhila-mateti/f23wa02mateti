7aa9e8cd550b5c22081c0c859782d3e5cfd8bc39
nikhila-mateti <s564202@nwmissouri.edu>
2023-10-21 23:59:32 -0500

    computation is added

--- routes/computation.js
+++ routes/computation.js
@@ -6,8 +6,8 @@ var  y = Math.random()
 /* GET home page. */
 router.get('/', function(req, res, next) {
     const pow = Math.pow(x,y)
-    res.send(pow)
- // res.render('computation', { title: 'Nikhila Mateti Bonus' });
+    
+  res.render('computation', { title: 'Nikhila Mateti Bonus' },{pow});
 });
 
 module.exports = router;
