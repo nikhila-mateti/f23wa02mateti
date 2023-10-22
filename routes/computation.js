f194b16d230c2703d48c30936367557a315e715a
nikhila-mateti <s564202@nwmissouri.edu>
2023-10-22 00:08:48 -0500

    computation is added

--- routes/computation.js
+++ routes/computation.js
@@ -2,12 +2,13 @@ var express = require('express');
 var router = express.Router();
 var  x = Math.random()
 var  y = Math.random()
-
+var pow = Math.pow(x,y)
+var sinh = Math.sinh(x)
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    const pow = Math.pow(x,y)
+   
     
-  res.render('computation', { title: 'Nikhila Mateti Bonus' });
+  res.render(`${pow},\n${sinh}`);
 });
 
 module.exports = router;
