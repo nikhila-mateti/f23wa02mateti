c6304459bb825a166a99eb18196294a7b314aad6
nikhila-mateti <s564202@nwmissouri.edu>
2023-10-24 00:01:29 -0500

    Computation changed

--- routes/computation.js
+++ routes/computation.js
@@ -3,9 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var  x = Math.random().toFixed(2)
-  var y = Math.random().toFixed(2)
-  
+  var  rand1 = Math.random().toFixed(2)
+  var rand2 = Math.random().toFixed(2)
+  var y = req.query.y;
+  var x = req.query.x;
+
+  if(y==undefined && x==undefined){
+    x = rand1
+    y = rand2
+  }
   pow = Math.pow(x,y).toFixed(2)
   trunc= Math.trunc(x).toFixed(2)
   tanh = Math.tanh(y).toFixed(2)
