eb6a24ba066981e76951631c7f74021ea710e821
nikhila-mateti <s564202@nwmissouri.edu>
2023-10-23 23:31:41 -0500

    Computation changed

--- routes/computation.js
+++ routes/computation.js
@@ -10,7 +10,7 @@ router.get('/', function(req, res, next) {
   trunc= Math.trunc(x).toFixed(2)
   tanh = Math.tanh(y).toFixed(2)
   sign = Math.sign(x).toFixed(2)
-  res.send(`Math.pow(${x},${y}): ${pow} Math.trunc(${x}) is: ${trunc} Math.tanh(${y}) is : ${tanh} Math.sign(${x}) is : ${sign}`);
+  res.send(`Math.pow(${x},${y}): ${pow} \nMath.trunc(${x}) is: ${trunc} \nMath.tanh(${y}) is : ${tanh} \nMath.sign(${x}) is : ${sign}`);
 });
 
 module.exports = router;
